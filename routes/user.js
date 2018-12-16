22db1cc323eef191c7e0da6a5e4b6874130d3874
OlfaKaroui <olfaakaroui@gmail.COM>
2018-12-16 02:04:11 +0100

    user

--- routes/user.js
+++ routes/user.js
@@ -7,12 +7,28 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: "localhost",
     user: "root",
-    database: "todo"
+    password:"mysql",
+    database: "blasti_bd"
 })
 
 function getConnection(){
     return pool
 }
+
+
+router.get("/login", (req, res) => {
+    console.log(req.params.email)
+    pool.query("SELECT * FROM users WHERE email = ? AND password = ?", [req.body.email, req.body.password], (err, rows) => {
+        if(err){
+            console.log(err)
+            res.sendStatus(500)
+            return
+        }
+        res.status(200)
+        res.json(rows)
+    })
+})
+
 
 
 
